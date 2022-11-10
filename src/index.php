5d73eb5153b62e0268fd3332c75a8bf6246068cb
TaniaAlcudia <tanialcudia@gmail.com>
2022-11-10 13:01:49 +0100

    view header

--- src/index.php
+++ src/index.php
@@ -2,8 +2,6 @@
 // Esta sentencia es obligatoria en cada archivo PHP de entrada (no views)
 require_once "app/__include.inc.php";
 
-// Para acceder a esta página se necesita autenticación
-needs_authentication();
-?>
 
-<h1>Index</h1>
+include "views/header.view.php";
+?>
