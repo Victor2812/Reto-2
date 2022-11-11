3d0a6c5edfd87f1ffd2b63388d3498c54f148369
Gaizka <gaizka.guerrero@ikasle.egibide.org>
2022-11-11 13:52:09 +0100

    feat(db-post): actualizar posts

--- src/app/db/repositories/post_repo.inc.php
+++ src/app/db/repositories/post_repo.inc.php
@@ -2,6 +2,15 @@
 
 
 abstract class PostRepository {
+    /**
+     * Crea un nuevo post
+     * @param string $title Título del post
+     * @param string $text Texto del post
+     * @param CategoryEntity $category Categoría del post
+     * @param UserEntity $author Autor del post
+     * @param array $tags Tags del post
+     * @return PostEntity|null
+     */
     public static function createNewPost(string $title, string $text, CategoryEntity $category, UserEntity $author, array $tags): PostEntity|null {
         global $db;
 
@@ -29,6 +38,9 @@ abstract class PostRepository {
         return null;
     }
 
+    /**
+     * Obtiene el objeto PostEntity a raíz de una fila obtenida de la sentencia SQL
+     */
     private static function getPostFromData(array $data): PostEntity|null {
         $categ = CategoryRepository::getCategoryById(intval($data['category']));
         $author = UserRepository::getUserById(intval($data['author']));
@@ -47,6 +59,11 @@ abstract class PostRepository {
         }
     }
 
+    /**
+     * Obtiene un post por su ID
+     * @param int $id ID del post
+     * @return PostEntity|null
+     */
     public static function getPostById(int $id): PostEntity|null {
         global $db;
 
@@ -65,6 +82,11 @@ abstract class PostRepository {
         return null;
     }
 
+    /**
+     * Obtiene un post por su ID
+     * @param int $id ID del post
+     * @return PostEntity|null
+     */
     public static function getPostsByAuthor(UserEntity $author): array {
         global $db;
 
@@ -87,6 +109,12 @@ abstract class PostRepository {
         return null;
     }
 
+    /**
+     * Obtener los posts paginados y ordenados por su fecha de creación
+     * @param int $offset A partir de qué post índice se quiere cargar
+     * @param int $limit Cuántos posts se quieren cargar
+     * @return array Array de posts
+     */
     public static function getLastPosts(int $offset = 0, int $limit = 15): array {
         global $db;
 
@@ -107,5 +135,33 @@ abstract class PostRepository {
         return $posts;
     }
 
-    // actualizar
+    /**
+     * Actualiza el post a la base de datos
+     * @param PostEntity $post Post a actulizar
+     */
+    public static function update(PostEntity $post) {
+        global $db;
+
+        $sql = 'UPDATE posts SET title=:title, text=:text, category=:categ, author=:author WHERE id = :id';
+    
+        $statement = $db->prepare($sql);
+        $statement->execute([
+            ':title' =>     $post->getTitle(),
+            ':text' =>      $post->getText(),
+            ':categ' =>     $post->getCategory()->getId(),
+            ':author' =>    $post->getAuthor()->getId(),
+            ':id' =>         $post->getId()
+        ]);
+
+        // Borrar todos los tags del post y aplicarlos de nuevo para actualizarlos
+        $db->prepare('DELETE FROM tagged WHERE post = :id')->execute([':id', $post->getId()]);
+        // Crear los enlaces a los tags
+        foreach ($post->getTags() as $tag) {
+            $s = $db->prepare("INSERT INTO tagged (tag, post) VALUES (:tag, :post)");
+            $s->execute([
+                ':tag' => $tag->getId(),
+                ':post' => $post->getId()
+            ]);
+        }
+    }
 }
