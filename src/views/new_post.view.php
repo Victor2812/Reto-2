202d68ab74047bc2a4cd91ac8e781db4adf2274d
TaniaAlcudia <tanialcudia@gmail.com>
2022-11-14 10:53:52 +0100

    aniadir post maquetado

--- src/views/new_post.view.php
+++ src/views/new_post.view.php
@@ -3,22 +3,29 @@
         <div class="new-post">
             <form action="" class="new-post-form">
                 <label for="new-post-category">Categoría</label>
-                <select name="new-post-category" id="">
-                    <option value="Category 1">Category 1</option>
-                    <option value="Category 2">Category 2</option>
-                    <option value="Category 3">Category 3</option>
-                    <option value="Category 4">Category 4</option>
-                    <option value="Category 5">Category 5</option>
-                </select>
+                <div class="select-category">
+                    <select name="new-post-category" id="new-post-category">
+                        <option value="logisitca">Logística</option>
+                        <option value="produccion">Producción</option>
+                        <option value="ingenieria">Ingeniería</option>
+                        <option value="calidad">Calidad</option>
+                        <option value="financiero">Financiero</option>
+                        <option value="otros">Otros</option>
+                    </select>
+                </div>
                 <label for="new-post-title">Título *</label>
-                <input type="text" name="new-post-title" id="" required>
-                <textarea name="new-post-text" id="" cols="30" rows="10" placeholder="Escribe aqui"></textarea>
-                <label for="new-post-upload">
-                    <?php include "img/upload.svg"; ?>
-                    Añadir adjunto
-                </label>
-                <input type="file" name="new-post-upload" id="">
-                <input class="button-blue" type="submit" value="Publicar">
+                <input type="text" name="new-post-title" id="new-post-title" required>
+                <label for="new-post-tags">Tags</label>
+                <input type="text" name="new-post-tags" id="new-post-tags" placeholder="tag1, tag2 ...">
+                <textarea name="new-post-text" id="new-post-text" cols="30" rows="10" placeholder="Escribe aqui"></textarea>
+                <div class="new-post-buttons">
+                    <label for="new-post-upload">
+                        <?php include "img/upload.svg"; ?>
+                        Añadir adjunto
+                    </label>
+                    <input type="file" id="new-post-upload" name="new-post-upload">
+                    <input class="button-blue" type="submit" value="Publicar">
+                </div>
             </form>
         </div>
     </main>
