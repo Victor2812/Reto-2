cd784d0e6081571707e811a4a62e74dc88f61548
Gaizka <gaizka.guerrero@ikasle.egibide.org>
2022-11-14 13:52:41 +0100

    cargar posts de forma dinamica

--- src/views/main_content.view.php
+++ src/views/main_content.view.php
@@ -1,7 +1,3 @@
-<!--- titulo>
-<user> en <categoria> <funcion fecha>
-<favs> <comentarios--->
-
 <div class="content">
     <aside class="sidebar-left">
 
@@ -9,10 +5,10 @@
     <main>
         <a id="newpostbtn" class="button-blue" href="/post_new.php"><i>#</i> Nuevo post</a>
         <div class="post-container">
-            <div class="post-partial"></div>
         </div>
     </main>
     <aside class="sidebar-right">
 
     </aside>
 </div>
+<script src="js/post.js"></script>
