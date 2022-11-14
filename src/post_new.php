e34c729557d00e9dccee761bd3ed3036e42caaea
Gaizka <gaizka.guerrero@ikasle.egibide.org>
2022-11-14 11:53:13 +0100

    pagina para nuevo post

--- src/post_new.php
+++ src/post_new.php
@@ -2,10 +2,12 @@
 // Esta sentencia es obligatoria en cada archivo PHP de entrada (no views)
 require_once "app/__include.inc.php";
 
+//necesita estar logeado para poder subir un post
 needs_authentication();
 
 include_views([
     "views/header.view.php",
+    "views/post_new.view.php",
     "views/footer.view.php"
 ]);
 ?>
