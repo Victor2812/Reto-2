0e701eb8ec510512c5c2d29cd9a8cc035f998ea6
Victor2812 <victor.ibanez@ikasle.egibide.org>
2022-11-15 14:04:59 +0100

    style of aside-left

---
+++ src/aside_right.php
@@ -0,0 +1,7 @@
+<?php 
+    //Esta sentencia es obligatoria en cada archivo PHP de entrada
+    require_once "app/__include.inc.php";
+
+    //Obtener parametros
+
+?>
--- src/views/main_content.view.php
+++ src/views/main_content.view.php
@@ -8,7 +8,32 @@
         </div>
     </main>
     <aside class="sidebar-right">
-
+        <div class="right_container">
+            <div class="aside_div">
+                <div class="titulo">PUNTOS</div>
+                <div class="points">87</div>
+            </div>
+            <div class="aside_div">
+                <div class="titulo">AMIGOS</div>
+                <div class="lista_amigos">
+                    <ul>
+                        <li><img src="../img/user-default-image.svg" alt="user_img"><p>miUsusario1</p></li>
+                        <li><img src="../img/user-default-image.svg" alt="user_img"><p>miUsuario1</p></li>
+                        <li><img src="../img/user-default-image.svg" alt="user_img"><p>miUsuario1</p></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="aside_div">
+                <div class="titulo">SEGUIDORES</div>
+                <div class="lista_amigos">
+                    <ul>
+                        <li><img src="../img/user-default-image.svg" alt="user_img"><p>miUsusario1</p></li>
+                        <li><img src="../img/user-default-image.svg" alt="user_img"><p>miUsuario1</p></li>
+                        <li><img src="../img/user-default-image.svg" alt="user_img"><p>miUsuario1</p></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </aside>
 </div>
 <script src="js/post.js"></script>
