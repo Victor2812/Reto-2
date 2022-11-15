470369958a1db4092dc8219549a0a76a2920b38f
imanolurquijo <imanol.urquijo@ikasle.egibide.org>
2022-11-15 11:53:13 +0100

    aside izquierdo terminado

--- src/views/main_content.view.php
+++ src/views/main_content.view.php
@@ -1,6 +1,26 @@
 <div class="content">
     <aside class="sidebar-left">
-
+        <div class="titulo">
+            <div class="titulo2">CATEGORÍAS</div>
+            <ul class="listaDepart">
+                 <il class="listaD">Produccion</il>
+                <il class="listaD">Ingenieria</il>
+                <il class="listaD">Logistica</il>
+            </ul>
+        </div>
+        <div class="titulo">
+            <div class="titulo2">TAGS</div>
+            <ul class="listaTag">
+                <il class="listaT"><a>Airbus320</a></il>
+                <il class="listaT">A330</il>
+                <il class="listaT">Airbus220</il>
+                <il class="listaT">Airbus620</il>
+                <il class="listaT">Airbus20</il>
+                <il class="listaT">Airbus300</il>
+                <il class="listaT">A220</il>
+                <il class="listaT">A200</il>
+            </ul>
+        </div>
     </aside>
     <main>
         <a id="newpostbtn" class="button-blue" href="/post_new.php"><i>#</i> Nuevo post</a>
