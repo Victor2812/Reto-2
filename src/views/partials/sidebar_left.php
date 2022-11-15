f4ca5c027a579d5a0150f9402ad5526559ad7fbc
Gaizka <gaizka.guerrero@ikasle.egibide.org>
2022-11-15 17:06:04 +0100

    fix: sidebar izquierdo

---
+++ src/views/partials/sidebar_left.php
@@ -0,0 +1,23 @@
+<aside class="sidebar-left">
+    <div>
+        <h3>CATEGORÍAS</h3>
+
+        <ul class="categories">
+            <?php foreach (CategoryRepository::getAllCategories() as $category): ?>
+                <li>
+                    <button class="category" data-id="<?php echo $category->getId(); ?>">
+                        <?php echo $category->getName(); ?>
+                    </button>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+    <div>
+        <h3>TAGS</h3>
+        <p>Obtener los tags más usados</p>
+        <p>Problema: el contador de los tags no está programado</p>
+
+        <ul>
+        </ul>
+    </div>
+</aside>
