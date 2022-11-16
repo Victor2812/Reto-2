e82bf6796de1feff8a0d58f7f7109a1abe59ea24
Victor2812 <victor.ibanez@ikasle.egibide.org>
2022-11-16 09:23:22 +0100

    puntuación del usuario funcional

--- src/aside_right.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    //Esta sentencia es obligatoria en cada archivo PHP de entrada
-    require_once "app/__include.inc.php";
-
-    //Obtener parametros
-
-?>
---
+++ src/views/aside_right.php
@@ -0,0 +1,26 @@
+<div class="right_container">
+    <div class="aside_div">
+        <div class="titulo">PUNTOS</div>
+        <div class="points"><?php echo $GLOBALS['session']->getCurrentUser()->getPoints() ?></div>
+    </div>
+    <div class="aside_div">
+        <div class="titulo">AMIGOS</div>
+        <div class="lista_amigos">
+            <ul>
+                <li><img src="../img/user-default-image.svg" alt="user_img"><p>miUsusario1</p></li>
+                <li><img src="../img/user-default-image.svg" alt="user_img"><p>miUsuario1</p></li>
+                <li><img src="../img/user-default-image.svg" alt="user_img"><p>miUsuario1</p></li>
+            </ul>
+        </div>
+    </div>
+    <div class="aside_div">
+        <div class="titulo">SEGUIDORES</div>
+        <div class="lista_amigos">
+            <ul>
+                <li><img src="../img/user-default-image.svg" alt="user_img"><p>miUsusario1</p></li>
+                <li><img src="../img/user-default-image.svg" alt="user_img"><p>miUsuario1</p></li>
+               <li><img src="../img/user-default-image.svg" alt="user_img"><p>miUsuario1</p></li>
+                </ul>
+            </div>
+        </div>
+</div>
