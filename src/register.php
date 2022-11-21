dad9b8591553f598457e803057c70eef912d34db
TaniaAlcudia <tanialcudia@gmail.com>
2022-11-21 10:13:17 +0100

    Maquetación login/register VT completada

--- src/register.php
+++ src/register.php
@@ -6,12 +6,11 @@ require_once "app/__include.inc.php";
 if (get_method() == 'POST'){
 
     //Comprobar la existencia de los datos
-    if (!check_post_data(['reg_username', 'reg_name', 'reg_pass', 'reg_rp_pass'])) {
+    if (!check_post_data(['reg_username', 'reg_pass', 'reg_rp_pass'])) {
         redirect(current_file());
     }
 
     $username = $_POST['reg_username'];
-    $name = $_POST['reg_name'];
     $password = $_POST['reg_pass'];
     $rp_password = $_POST['reg_rp_pass'];
 
@@ -21,7 +20,7 @@ if (get_method() == 'POST'){
     }
 
     //Crear nuevo usuario y autenticar
-    if ($user = UserRepository::createNewUser($username, $name, null, null, $password)) {
+    if ($user = UserRepository::createNewUser($username, null, null, null, $password)) {
         $session->authenticate($user);
         redirect('/index.php');
     }
@@ -30,7 +29,7 @@ if (get_method() == 'POST'){
     redirect(current_file());
 } else {
     include_views([
-        "views/register.view.php"
+        "views/login.view.php"
     ]);
 }
 
--- src/views/login.view.php
+++ src/views/login.view.php
@@ -4,22 +4,22 @@
         <div class="login-container">
             <input type="checkbox" id="chk" aria-hidden="true">
             <img src="./img/aerbide-logo.svg" alt="logo-aerbide">
-            <div class="signup">
-                <form action="<?php echo current_file(); ?>" method="POST">
-                    <label for="chk" aria-hidden="true">Sign up</label>
-                    <input type="text" name="txt" placeholder="Usuario" required>
-                    <input type="password" name="pass" placeholder="Contraseña" required>
-                    <input type="password" name="pass-repeat" placeholder="Repite Contraseña" required>
-                    <button class="button-blue">Sign up</button>
-                </form>
-            </div>
             <div class="login">
                 <form action="<?php echo current_file(); ?>" method="POST">
-                    <label for="chk" aria-hidden="true">Login</label>
-                    <input type="text" name="txt" placeholder="Usuario" required>
-                    <input type="password" name="pass" placeholder="Contraseña" required>
-                    <button class="button-blue">Login</button>
+                    <input type="text" name="login_user" placeholder="Usuario" required>
+                    <input type="password" name="login_pass" placeholder="Contraseña" required>
+                    <input type="submit" class="button-blue" value="Login">
+                    <label for="chk" aria-hidden="true">¿No tienes cuenta?</label>
                 </form>        
+            </div>
+            <div class="signup">
+                <form action="<?php echo current_file(); ?>" method="POST">
+                    <input type="text" name="reg_username" placeholder="Usuario" required>
+                    <input type="password" name="reg_pass" placeholder="Contraseña" required>
+                    <input type="password" name="reg_rp_pass" placeholder="Repite Contraseña" required>
+                    <input type="submit" class="button-blue" value="Regístrate">
+                    <label for="chk" aria-hidden="true">Ya tengo cuenta</label>
+                </form>
             </div>
         </div>
     </main>
