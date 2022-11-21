aab788185762a5f9029d97653fbbb8fddaa94657
TaniaAlcudia <tanialcudia@gmail.com>
2022-11-21 08:46:32 +0100

    login

--- src/views/login.view.php
+++ src/views/login.view.php
@@ -1,38 +1,28 @@
-<div class="login-wrapper">
-    <div class="login-container">
-        <div class="logo">
-            <img src="../img/aerbide-logo.svg" alt="logo">
+
+<div class="login-screen">
+    <main>
+        <div class="login-container">
+            <input type="checkbox" id="chk" aria-hidden="true">
+            <img src="./img/aerbide-logo.svg" alt="logo-aerbide">
+            <div class="signup">
+                <form action="<?php echo current_file(); ?>" method="POST">
+                    <label for="chk" aria-hidden="true">Sign up</label>
+                    <input type="text" name="txt" placeholder="Usuario" required>
+                    <input type="password" name="pass" placeholder="Contraseña" required>
+                    <input type="password" name="pass-repeat" placeholder="Repite Contraseña" required>
+                    <button class="button-blue">Sign up</button>
+                </form>
+            </div>
+            <div class="login">
+                <form action="<?php echo current_file(); ?>" method="POST">
+                    <label for="chk" aria-hidden="true">Login</label>
+                    <input type="text" name="txt" placeholder="Usuario" required>
+                    <input type="password" name="pass" placeholder="Contraseña" required>
+                    <button class="button-blue">Login</button>
+                </form>        
+            </div>
         </div>
-        <form class="form-login" action="<?php echo current_file(); ?>" method="POST">
-            <p>Inicia sesión con tu cuenta</p>
-            <div class="login">
-                <div class="icon">
-                    <?php include "img/user.svg" ?>
-                </div>
-                <div class="in">
-                    <input type="text" placeholder="Usuario" name="login_user">
-                </div>
-                <div class="icon">
-                    <?php include "img/uncheck.svg"; ?>
-                </div>
-            </div>
-            <div class="login">
-                <div class="icon">
-                    <?php include "img/password.svg" ?>
-                </div>
-                <div class="in">
-                    <input type="password" placeholder="Password" name="login_pass">
-                </div>
-                <div class="icon">
-                    <?php include "img/uncheck.svg"; ?>
-                </div>
-            </div>
-            <div class="register">
-                <a href="/register.php">¿No tienes cuenta?</a>
-            </div>
-            <div>
-                <input type="submit" value="Iniciar sesión">
-            </div>
-        </form>
-    </div>
+    </main>
 </div>
+
+
