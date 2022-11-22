b99ab0a86d57cb992b28f49d3279316231c36d0a
Gaizka <gaizka.guerrero@ikasle.egibide.org>
2022-11-22 13:18:11 +0100

    feat: language selector

--- src/js/views/partials/header.js
+++ src/js/views/partials/header.js
@@ -1,3 +1,13 @@
 window.addEventListener('load', () => {
-    
+    const LANG_COOKIE = 'aerbidelang';
+
+    let selector = document.querySelector('#langsel');
+
+    selector.addEventListener('change', () => {
+        let lang = selector.options[selector.selectedIndex].value;
+
+        // crar la cookie de idioma que no expire
+        // el idioma es para la máquina, independientemente del usuario
+        document.cookie = `${LANG_COOKIE}=${lang}`;
+    });
 });
--- src/views/header.view.php
+++ src/views/header.view.php
@@ -1,3 +1,10 @@
+<?php
+    // seleccionar el idioma actual
+    $currentLang = isset($_COOKIE[LANGUAGE_COOKIE])
+        ? $_COOKIE[LANGUAGE_COOKIE]
+        : 'ES';
+?>
+
 <header>
     <a href="/index.php"><img class="logo" src="img/aerbide-logo.svg" alt="logo-aerbide"></a>
     <div class="navigation">
@@ -15,13 +22,20 @@
                 </form>
             </div>
             <div class="user">
-                <button id="notifbtn">
-                    <?php include "img/notification-white-stroke.svg"; ?>
-                </button><button id="userbtn">
+                <select id="langsel">
+                    <?php foreach (LANGUAGES as $lang => $name): ?>
+                        <option 
+                            value="<?php echo $lang; ?>"
+                            <?php if ($lang == $currentLang) { echo 'selected'; } ?>
+                        ><?php echo $name; ?></option>
+                    <?php endforeach; ?>
+                </select>
+                <button id="userbtn">
                     <?php include "img/user-default-image.svg"; ?>
                 </button>
             </div>
         </div>
     </div>
 </header>
+<script src="js/views/partials/header.js"></script>
 <?php include "partials/user_banner.php"; ?>
