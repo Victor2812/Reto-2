4bd374950870ef4641921752fef4aff70b12ad81
TaniaAlcudia <tanialcudia@gmail.com>
2022-11-24 13:47:24 +0100

    maquetación ranking screen

--- src/views/ranking.view.php
+++ src/views/ranking.view.php
@@ -6,27 +6,26 @@
     <main>
         <div class="ranking-content">
             <h2  class="title">Ranking de Usuarios</h2>
+            <div class="grid-container title">
+                <p>Posicion</p>
+                <p>Imagen</p>
+                <p>Usuario</p>
+                <p>Puntos</p>
+            </div>
             <ol class="ranking">
-            <li>
-                <div class="flex-container">
-                    <div class="title1"><span class="t1" data-desktop="Posicion" data-movil="Pos"></sapn></div>
-                    <div class="title2">Imagen</div>
-                    <div class="title1">Usuario</div>
-                    <div class="title2">Puntos</div>
-                </div>
-            </li>
                 <?php for ($i=0; $i < count($list); $i++): ?>
                     <li class="<?php 
                             if ($GLOBALS['session']->getCurrentUser()->getId() == $list[$i]->getId()) {
                                 echo 'current';
                             }
                         ?>">
-                        <div class="flex-container">
+                        <div class="grid-container">
                             <span class="pos"><?php echo $i + 1; ?></span>
                             <img class="author-icon" src="img/user-default-image.svg" alt="Imágen del usuario">
-                            <h2><a href="user.php?user=<?php echo $list[$i]->getId(); ?>"><?php echo $list[$i]->getUsername(); ?></a></h2>
+                            <p><a href="user.php?user=<?php echo $list[$i]->getId(); ?>"><?php echo $list[$i]->getUsername(); ?></a></p>
                             <span><?php echo $list[$i]->getPoints(); ?> puntos</span>
                         </div>
+                           
                     </li>
                 <?php endfor; ?>
             </ol>
